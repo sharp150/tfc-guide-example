1200e764d77e902a17a2e3612164ad72fbd170dc
wayne <59746733+sharp150@users.noreply.github.com>
2021-12-21 11:22:57 +0000

    tidy

--- variables.tf
+++ variables.tf
@@ -2,7 +2,6 @@ variable "region" {
   description = "AWS region"
   default     = "us-west-1"
 }
-
 variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t2.micro"
